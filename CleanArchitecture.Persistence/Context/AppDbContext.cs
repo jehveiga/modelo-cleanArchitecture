530f93724e0d100f3620f24c25066ddc2a79db9d
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-09-06 19:40:45 -0300

    Adicionar a classe DbContext

---
+++ CleanArchitecture.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<User> Users { get; set; }
+    }
+}
