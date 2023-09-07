36fcd4b57645125c0b9d5d0e1907b03e027966c4
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-09-06 21:46:55 -0300

    Adicionar a string de conexão no arquivo appsettings e a classe que extende IServiceCollection para efetuar as injeções de dependência

---
+++ CleanArchitecture.Persistence/Repositories/UnitOfwork.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+
+namespace CleanArchitecture.Persistence.Repositories
+{
+    // Classe Responsável por controlar a transmissão de dados para o Data Base, centralizando suas transmissões de dados somente nela
+    // usando sempre o mesmo contexto por meio do tempo de vida Scoped definido na injeção pela a mesma operação usada nas outras classes que utilizam DbContext
+    public class UnitOfwork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        public UnitOfwork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Commit(CancellationToken cancellationToken)
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
